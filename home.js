bf17f993e73cdbfc65fcd098aa03ecfe05f04f06
Saif uddin Babar <mdbabar4202@gmail.com>
2025-08-28 20:20:26 +0600

    call button workable with js

--- home.js
+++ home.js
@@ -19,6 +19,55 @@ for (const btn of btns) {
     btn.style.backgroundColor = "#16a34a";
   });
 }
+// call btn 
+document.getElementById('cardbox').addEventListener("click", function(e) {
+    if (e.target.classList.contains("btn2")) {
+        const maincoin = getElement("coin");
+let coinValue = Number(maincoin.innerText); 
+
+const totalcoin = coinValue - 20;
+
+if (totalcoin< 0) {
+    alert("Not enough coins");
+    return;
+} 
+     
+             const card = e.target.parentNode.parentNode.children[1]
+    const contact =  e.target.parentNode.parentNode.children[2]
+  
+
+        
+             const title = card.querySelector(".title").innerText
+        const contactNUmber = contact.querySelector(".contact").innerText
+       
+
+
+        alert(title+ " " +contactNUmber);
+      
+      maincoin.innerText = totalcoin; 
+      const container = getElement('containerforhistory')
+
+    const newdiv = document.createElement('div')
+newdiv.innerHTML = `
+<div class="flex justify-between border border-gray-200 rounded-xl p-5">
+        <div>
+          <h1 class="font-bold">${title}</h1>
+          <p class="font-bold text-black-800">${contactNUmber}</p>
+        </div>
+        <div>
+         <p>${new Date().toLocaleTimeString()}</p>
+        </div>
+
+</div>
+      
+      `
+
+container.appendChild(newdiv)
+
+
+    }
+});
+
 
 
 
